71b54fa3f8e4c6dbaa247eefe54e8c7e91fe70bd
ZhouDanni <1833762599@qq.com>
2022-03-21 15:20:22 +0800

    新增所有敌机的抽象父类

---
+++ src/edu/hitsz/aircraft/AbstractEnemy.java
@@ -0,0 +1,34 @@
+package edu.hitsz.aircraft;
+
+import edu.hitsz.application.Main;
+import edu.hitsz.bullet.AbstractBullet;
+
+import java.util.List;
+
+/**
+ * 所有敌机的抽象父类（MOB,ELITE,BOSS）
+ * @author Zhoudanni
+ */
+public abstract class AbstractEnemy extends AbstractAircraft{
+
+    public AbstractEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
+        super(locationX, locationY, speedX, speedY, hp);
+    }
+
+    @Override
+    public List<AbstractBullet> shoot() {
+        return null;
+    }
+
+    /**
+     * 重写forward方法控制敌机的移动
+     */
+    @Override
+    public void forward() {
+        super.forward();
+        // 判定 y 轴向下飞行出界
+        if (locationY >= Main.WINDOW_HEIGHT ) {
+            vanish();
+        }
+    }
+}
