fe7ed247182d2a6d1cb58d23af772b455d3e4b34
ZhouDanni <1833762599@qq.com>
2022-03-29 15:12:39 +0800

    新增精英敌机掉落道具方法，减少Game端耦合度

--- src/edu/hitsz/aircraft/EliteEnemy.java
+++ src/edu/hitsz/aircraft/EliteEnemy.java
@@ -1,10 +1,12 @@
 package edu.hitsz.aircraft;
+import edu.hitsz.booster.*;
 import edu.hitsz.bullet.AbstractBullet;
 import edu.hitsz.bullet.EnemyBullet;
 
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * 精英敌机类
@@ -46,5 +48,38 @@ public class EliteEnemy extends AbstractEnemy {
         abstractBullet = new EnemyBullet(x, y, speedX, speedY, power);
         res.add(abstractBullet);
         return res;
+    }
+
+    private PropFactory propFactory;
+
+    /**
+     * 精英敌机掉落道具
+     *
+     * @return 返回某种道具
+     */
+    public AbstractBoosterPacks createProp(){
+        Random r = new Random();
+        double randomNumber = r.nextDouble();
+        double portion = 0.5;
+        if(randomNumber < portion){
+            int propRandom = r.nextInt(3);
+            switch (propRandom) {
+                // 加血道具
+                case 0:
+                    propFactory = new PropBloodFactory();
+                    return propFactory.createOperation(this.getLocationX(), this.getLocationY());
+                // 火力道具
+                case 1:
+                    propFactory = new PropBulletFactory();
+                    return propFactory.createOperation(this.getLocationX(), this.getLocationY());
+                // 炸弹道具
+                case 2:
+                    propFactory = new PropBombFactory();
+                    return propFactory.createOperation(this.getLocationX(), this.getLocationY());
+                default:
+                    throw new IllegalStateException("Unexpected value: " + propRandom);
+            }
+        }
+        return null;
     }
 }
