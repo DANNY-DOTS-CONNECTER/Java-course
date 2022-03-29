a6f92f0fadb5ef5bcc14011630f3e0bd544213c5
ZhouDanni <1833762599@qq.com>
2022-03-29 10:29:17 +0800

    修改注释

--- src/edu/hitsz/aircraft/BossEnemyFactory.java
+++ src/edu/hitsz/aircraft/BossEnemyFactory.java
@@ -5,14 +5,23 @@ import edu.hitsz.application.Main;
 
 import static edu.hitsz.application.Main.WINDOW_WIDTH;
 
-public class BossEnemyFactory implements EnemyFactory{
+/**
+ * BOSS敌机工厂类
+ *
+ * @author Zhoudanni
+ */
+public class BossEnemyFactory implements EnemyFactory {
 
-    int X = Math.random() >= 0.5 ? 1 : -1;//产生横向速度方向的变量
+    /**
+     * 产生横向速度方向的变量
+     */
+    int speedX = Math.random() >= 0.5 ? 1 : -1;
+
     @Override
     public AbstractEnemy createOperation() {
         return new BossEnemy((int) (Math.random() * (WINDOW_WIDTH - ImageManager.BOSS_ENEMY_IMAGE.getWidth())),
                 (int) (Math.random() * Main.WINDOW_HEIGHT * 0.2),
-                X,
+                speedX,
                 0,
                 60);
     }
--- src/edu/hitsz/aircraft/EnemyFactory.java
+++ src/edu/hitsz/aircraft/EnemyFactory.java
@@ -1,5 +1,15 @@
 package edu.hitsz.aircraft;
 
+/**
+ * 敌机总工厂类
+ *
+ * @author Zhoudanni
+ */
 public interface EnemyFactory {
+    /**
+     * 敌机的总工厂方法
+     *
+     * @return 返回一个抽象飞机类型
+     */
     AbstractEnemy createOperation();
 }
