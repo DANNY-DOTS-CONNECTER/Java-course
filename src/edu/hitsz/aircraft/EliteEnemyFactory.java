8dbe2f8550db2ad1b25d998b5bdf43576df9cfab
ZhouDanni <1833762599@qq.com>
2022-03-29 10:03:50 +0800

    修改精英敌机横向飞行方向

--- src/edu/hitsz/aircraft/EliteEnemyFactory.java
+++ src/edu/hitsz/aircraft/EliteEnemyFactory.java
@@ -5,14 +5,28 @@ import edu.hitsz.application.Main;
 
 import static edu.hitsz.application.Main.WINDOW_WIDTH;
 
-public class EliteEnemyFactory implements EnemyFactory{
-
-    int X = Math.random() >= 0.5 ? 3 : -3;//产生横向速度方向的变量
+/**
+ * 精英敌机工厂类
+ *
+ * @author Zhoudanni
+ */
+public class EliteEnemyFactory implements EnemyFactory {
 
     @Override
     public AbstractEnemy createOperation() {
+        int speedX = 0;
+        double num = Math.random();
+        double por1 = 0.7;
+        double por2 = 0.5;
+        if (num >= por1) {
+            speedX = 3;
+        } else if (num > por2) {
+            speedX = -3;
+        }
+
         return new EliteEnemy((int) (Math.random() * (WINDOW_WIDTH - ImageManager.ELITE_ENEMY_IMAGE.getWidth())),
-                (int) (Math.random() * Main.WINDOW_HEIGHT * 0.2), X,
+                (int) (Math.random() * Main.WINDOW_HEIGHT * 0.2), speedX,
                 8, 60);
     }
+
 }
