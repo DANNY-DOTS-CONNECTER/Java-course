00fdf03a5f30f481129e2a38b4cd9a376af8a523
ZhouDanni <1833762599@qq.com>
2022-04-03 20:26:15 +0800

    新增英雄机测试方法，包含getInstanceTest，decreaseHpTest，shootTest

---
+++ test/edu/hitsz/aircraft/HeroAircraftTest.java
@@ -0,0 +1,39 @@
+package edu.hitsz.aircraft;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroAircraftTest {
+    HeroAircraft heroAircraft;
+    HeroAircraft heroAircraft2;
+
+    @Test
+    @DisplayName("getInstance_test")
+    void getInstanceTest() {
+        heroAircraft = HeroAircraft.getInstance();
+        heroAircraft2 = HeroAircraft.getInstance();
+        assertEquals(heroAircraft, heroAircraft2);
+    }
+
+    @ParameterizedTest
+    @DisplayName("decrease_hp_test")
+    @CsvSource({"100,90", "10,0", "5,0", "-10,0"})
+    void decreaseHpTest(int num1, int num2) {
+        heroAircraft = HeroAircraft.getInstance();
+        heroAircraft.setHp(num1);
+        heroAircraft.decreaseHp(10);
+        assertEquals(num2, heroAircraft.getHp());
+    }
+
+    @Test
+    @DisplayName("HeroAircraft_shoot_Test")
+    void shootTest() {
+        heroAircraft = HeroAircraft.getInstance();
+        assertNotNull(heroAircraft.shoot());
+    }
+}
