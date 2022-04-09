ffe688a282005433f1755dd0686501d5dcb1b1fb
ZhouDanni <1833762599@qq.com>
2022-04-09 23:35:58 +0800

    新增boss机shootStrategy，创建新方法createProp()

--- src/edu/hitsz/aircraft/BossEnemy.java
+++ src/edu/hitsz/aircraft/BossEnemy.java
@@ -1,8 +1,10 @@
 package edu.hitsz.aircraft;
 
+import edu.hitsz.booster.*;
 import edu.hitsz.bullet.AbstractBullet;
-
+import edu.hitsz.trajectory.Strategy;
 import java.util.List;
+import java.util.Random;
 
 /**
  * BOSS敌机类
@@ -10,8 +12,10 @@ import java.util.List;
  * @author Zhoudanni
  */
 public class BossEnemy extends AbstractEnemy {
+    private Strategy strategy;
 
-    //暂时没有使用该类
+    private int shootNum = 3;
+
     /**
      * 子弹伤害
      * Boss敌机设定为40
@@ -24,13 +28,47 @@ public class BossEnemy extends AbstractEnemy {
      */
     private final int direction = 1;
 
-    public BossEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
+    public BossEnemy(int locationX, int locationY, int speedX, int speedY, int hp, Strategy strategy) {
         super(locationX, locationY, speedX, speedY, hp);
+        this.strategy = strategy;
+    }
+
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
     }
 
     @Override
     public List<AbstractBullet> shoot() {
-        return null;
+        return strategy.selectTrajectory(locationX, locationY, shootNum, direction, power);
+    }
+
+
+    /**
+     * BOSS敌机掉落道具
+     *
+     * @return 返回某种道具
+     */
+    public AbstractBoosterPacks createProp(){
+        PropFactory propFactory;
+        Random r = new Random();
+        int propRandom = r.nextInt(3);
+        switch (propRandom) {
+            // 加血道具
+            case 0:
+                propFactory = new PropBloodFactory();
+                return propFactory.createOperation(this.getLocationX(), this.getLocationY());
+            // 火力道具
+            case 1:
+                propFactory = new PropBulletFactory();
+                return propFactory.createOperation(this.getLocationX(), this.getLocationY());
+            // 炸弹道具
+            case 2:
+                propFactory = new PropBombFactory();
+                return propFactory.createOperation(this.getLocationX(), this.getLocationY());
+            default:
+                throw new IllegalStateException("Unexpected value: " + propRandom);
+        }
     }
 
 }
