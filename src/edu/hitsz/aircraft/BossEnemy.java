7048c62d2478e9b3bc31a4e43d49a4968ea38ca4
ZhouDanni <1833762599@qq.com>
2022-04-20 23:23:57 +0800

    删除子类中shoot实现，删除不必要成员属性，修改boss敌机返回的道具包为List类

--- src/edu/hitsz/aircraft/BossEnemy.java
+++ src/edu/hitsz/aircraft/BossEnemy.java
@@ -1,8 +1,9 @@
 package edu.hitsz.aircraft;
 
 import edu.hitsz.booster.*;
-import edu.hitsz.bullet.AbstractBullet;
-import edu.hitsz.trajectory.Strategy;
+import edu.hitsz.trajectory.ScatteredTrajectory;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,63 +13,47 @@ import java.util.Random;
  * @author Zhoudanni
  */
 public class BossEnemy extends AbstractEnemy {
-    private Strategy strategy;
 
-    private int shootNum = 3;
-
-    /**
-     * 子弹伤害
-     * Boss敌机设定为40
-     */
-    private final int power = 40;
-
-    /**
-     * 子弹射击方向 (向上发射：1，向下发射：-1)
-     * 此处定义为向上发射（相对y轴）
-     */
-    private final int direction = 1;
-
-    public BossEnemy(int locationX, int locationY, int speedX, int speedY, int hp, Strategy strategy) {
+    public BossEnemy(int locationX, int locationY, int speedX, int speedY, int hp) {
         super(locationX, locationY, speedX, speedY, hp);
-        this.strategy = strategy;
+        this.strategy = new ScatteredTrajectory();
+        this.shootNum = 3;
+        this.power = 40;
+        this.direction = 1;
     }
-
-
-    public void setStrategy(Strategy strategy) {
-        this.strategy = strategy;
-    }
-
-    @Override
-    public List<AbstractBullet> shoot() {
-        return strategy.selectTrajectory(locationX, locationY, shootNum, direction, power);
-    }
-
 
     /**
      * BOSS敌机掉落道具
      *
      * @return 返回某种道具
      */
-    public AbstractBoosterPacks createProp(){
+    public List<AbstractBoosterPacks> createProp() {
         PropFactory propFactory;
+        List<AbstractBoosterPacks> arrayList = new ArrayList<>();
         Random r = new Random();
-        int propRandom = r.nextInt(3);
-        switch (propRandom) {
-            // 加血道具
-            case 0:
-                propFactory = new PropBloodFactory();
-                return propFactory.createOperation(this.getLocationX(), this.getLocationY());
-            // 火力道具
-            case 1:
-                propFactory = new PropBulletFactory();
-                return propFactory.createOperation(this.getLocationX(), this.getLocationY());
-            // 炸弹道具
-            case 2:
-                propFactory = new PropBombFactory();
-                return propFactory.createOperation(this.getLocationX(), this.getLocationY());
-            default:
-                throw new IllegalStateException("Unexpected value: " + propRandom);
+        int propNum = 3;
+        for (int i = 0; i < propNum; i++) {
+            int propRandom = r.nextInt(3);
+            switch (propRandom) {
+                // 加血道具
+                case 0:
+                    propFactory = new PropBloodFactory();
+                    arrayList.add(propFactory.createOperation(this.getLocationX() + r.nextInt(50), this.getLocationY() - r.nextInt(50)));
+                    break;
+                // 火力道具
+                case 1:
+                    propFactory = new PropBulletFactory();
+                    arrayList.add(propFactory.createOperation(this.getLocationX() + r.nextInt(50), this.getLocationY() + r.nextInt(50)));
+                    break;
+                // 炸弹道具
+                case 2:
+                    propFactory = new PropBombFactory();
+                    arrayList.add(propFactory.createOperation(this.getLocationX() - r.nextInt(50), this.getLocationY() - r.nextInt(50)));
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected value: " + propRandom);
+            }
         }
+        return arrayList;
     }
-
 }
