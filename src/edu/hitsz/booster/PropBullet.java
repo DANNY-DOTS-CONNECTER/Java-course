3d1eca054030a9927ef1637fc3c02e7165043cc7
ZhouDanni <1833762599@qq.com>
2022-04-28 10:33:42 +0800

    修改子弹道具为可以叠加

--- src/edu/hitsz/booster/PropBullet.java
+++ src/edu/hitsz/booster/PropBullet.java
@@ -15,16 +15,19 @@ public class PropBullet extends AbstractBoosterPacks{
 
     @Override
     public void bonus(HeroAircraft heroAircraft) {
-        //TODO 英雄机碰到道具改变弹道
-        int curShootNum = heroAircraft.getShootNum();
+
         heroAircraft.setStrategy(new ScatteredTrajectory());
         //设置最大子弹数量为3
         int maxShootNum = 3;
+            int curShootNum = heroAircraft.getShootNum();
         if(curShootNum < maxShootNum){
             heroAircraft.increaseShootNum();
             System.out.println("increase 1 bullet");
         }else{
             System.out.println("reached the maximum shoot number");
         }
+//        heroAircraft.setShootNum(maxShootNum);
     }
 }
+
+
