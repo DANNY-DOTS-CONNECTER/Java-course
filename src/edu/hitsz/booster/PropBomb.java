e6932d08c826e155a4a19b5eeef0e7e0cb614c9d
ZhouDanni <1833762599@qq.com>
2022-05-04 00:09:26 +0800

    添加观察者模式

--- src/edu/hitsz/booster/PropBomb.java
+++ src/edu/hitsz/booster/PropBomb.java
@@ -1,6 +1,10 @@
 package edu.hitsz.booster;
 
 import edu.hitsz.aircraft.HeroAircraft;
+import edu.hitsz.observer.BombAction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 加炸弹包类
@@ -8,12 +12,29 @@ import edu.hitsz.aircraft.HeroAircraft;
  */
 public class PropBomb extends AbstractBoosterPacks{
 
+    private List<BombAction> listenerList = new ArrayList<>();
+
     public PropBomb(int locationX, int locationY, int speedX, int speedY) {
         super(locationX, locationY, speedX, speedY);
+    }
+
+    public void addListener(BombAction listener){
+        listenerList.add(listener);
+    }
+
+    public void removeListener(BombAction listener){
+        listenerList.remove(listener);
+    }
+
+    public void notifyAllListener(){
+        for(BombAction listener : listenerList){
+            listener.update();
+        }
     }
 
     @Override
     public void bonus(HeroAircraft aircraft) {
         System.out.println("BombSupply active!");
+        notifyAllListener();
     }
 }
